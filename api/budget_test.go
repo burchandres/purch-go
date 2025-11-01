e752d1a46cdcf97e0581b2e79147e46b93a1a910
Andres Burch <acburch22@gmail.com>
2025-11-01 15:24:50 -0400

    all /budget testing done and passes

--- api/budget_test.go
+++ api/budget_test.go
@@ -33,3 +33,45 @@ func TestGetCategories(t *testing.T) {
 	// check we get expected categories response for test data
 	assert.Equal(t, 3, len(categories))
 }
+
+func TestGetItems(t *testing.T) {
+	cookie := loginAndGetCookie(t, testuser, testpass)
+	// hit /budget/categories
+	resp := makeAuthenticatedRequest(t, client, "GET", budgetServiceUrl + "/items", nil, cookie)
+	// parse the response
+	body, err := io.ReadAll(resp.Body)
+	var items []database.Item
+	err = json.Unmarshal(body, &items)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	// check we get expected items response for test data
+	assert.Equal(t, 2, len(items))
+}
+
+func TestGetAccounts(t *testing.T) {
+	cookie := loginAndGetCookie(t, testuser, testpass)
+	// hit /budget/categories
+	resp := makeAuthenticatedRequest(t, client, "GET", budgetServiceUrl + "/accounts", nil, cookie)
+	// parse the response
+	body, err := io.ReadAll(resp.Body)
+	var accounts []database.Account
+	err = json.Unmarshal(body, &accounts)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	// check we get expected accounts response for test data
+	assert.Equal(t, 2, len(accounts))
+}
+
+func TestGetTransactions(t *testing.T) {
+	cookie := loginAndGetCookie(t, testuser, testpass)
+	// hit /budget/categories
+	resp := makeAuthenticatedRequest(t, client, "GET", budgetServiceUrl + "/transactions", nil, cookie)
+	// parse the response
+	body, err := io.ReadAll(resp.Body)
+	var transactions []database.Transaction
+	err = json.Unmarshal(body, &transactions)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+	// check we get expected transactions response for test data
+	assert.Equal(t, 3, len(transactions))
+}
