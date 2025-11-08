b3f968984cf4aa3a377e30b475b34142d99e7db5
Andres Burch <acburch22@gmail.com>
2025-11-08 10:53:54 -0500

    added plaid struct for parsing plaid webhook payload

--- webhook/webhook.go
+++ webhook/webhook.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	// "github.com/plaid/plaid-go/v40/plaid"
 
 	"purch/internal/config"
 )
@@ -28,6 +29,25 @@ func GetWebhookServer(config *config.Config) *http.Server {
 	}
 }
 
+/*
+Below is an example of the plaid.SyncUpdatesAvailableWebhook message format we'd expect:
+{
+  "webhook_type": "TRANSACTIONS",
+  "webhook_code": "SYNC_UPDATES_AVAILABLE",
+  "item_id": "wz666MBjYWTp2PDzzggYhM6oWWmBb",
+  "initial_update_complete": true,
+  "historical_update_complete": false,
+  "environment": "production"
+}
+*/
+
+// TODO: Follow this guide for verifying plaid webhooks: https://plaid.com/docs/api/webhooks/webhook-verification/
+func plaidAuthWebhookMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
 func servePlaidWebhook(c *gin.Context) {
+	// config := config.GetConfig()
+	
 	c.JSON(http.StatusOK, "To be implemented...")
 }
